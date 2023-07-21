94d2a41a4c6acee84f67e7a7a40fdef8096e306f
Johan Tibbelin <johan.roos@tibbelin.com>
2023-07-21 22:11:30 +0200

    Test sending "Stuff!" working

--- fujinet-bridge/netsiohub/fnmidi.py
+++ fujinet-bridge/netsiohub/fnmidi.py
@@ -1,5 +1,23 @@
-# Midi
+# Simple midi recieve test program using mido
+# 
+# Written by: Johan Tibbelin
 
 import mido
 
-fn midi_setup():
+inport = mido.open_input()
+s=""
+b = []
+for j in range(50):
+    for i in range(3):
+        msg = inport.receive()
+
+        b = b + msg.bytes()
+    
+    
+    
+    # for i
+    print(s.join(map(chr,b)))
+    b = []
+    s = ""
+# for j
+inport.close()
